434ee513477f31160cb03c6957276ef149bcb46f
mo-mosverkstad <34004324+mo-mosverkstad@users.noreply.github.com>
2017-12-16 13:15:53 +0100

    simple_grafteori: eulerian, first draft
    My first python practice trial for eulerian path algorithm

---
+++ raw/eulerian/eulerian.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+graph = {1:[2,3,5],2:[1,3],3:[1,4,5],4:[2,3], 5:[1,3]}
+
+def isodd(n):
+    return n % 2 == 1
+
+def check_eulerian(g):
+    # odd_vertex_list = [v for v, n in g.iteritems() if isodd(len(n))]
+    odd_vertex_list = []
+    for vertex, neighbors in g.iteritems():
+        if isodd(len(neighbors)):
+            odd_vertex_list.append(vertex)
+    if len(odd_vertex_list) == 0 or len(odd_vertex_list) == 2:
+        return True,odd_vertex_list
+    else:
+        return False,[]
+
+print check_eulerian(graph)
